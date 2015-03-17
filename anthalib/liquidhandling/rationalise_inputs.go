dd5859853363e634e689222b3e03cec2fd699113
Michael Sadowski <m.sadowski@synthace.com>
2015-03-17 16:57:18 +0000

    Added some logic to ensure inputs are properly sorted out

--- anthalib/liquidhandling/rationalise_inputs.go
+++ anthalib/liquidhandling/rationalise_inputs.go
@@ -1,13 +1,25 @@
 package liquidhandling
 
-func Rationalise_Inputs(lhr *LHRequest, lhp *LHProperties, inputs ...*LHSolution) *LHRequest {
+import "reflect"
+
+func Rationalise_Inputs(lhr *LHRequest, lhp *LHProperties, inputs ...*LHComponent) *LHRequest {
+
+	ip := make(map[string]*LHPlate)
 
 	for i := 0; i < len(inputs); i++ {
 		// need to check that the inputs are in the right format
 		// at the moment it's just a case of figuring out if they're in plates
 		// already... if not we panic
 
+		if reflect.TypeOf(inputs[i].Container).Name() != "*liquidhandling.LHWell" {
+			panic("Cannot use non-microplate formats in liquid handler... yet!")
+		}
+
+		ip[inputs[i].Container.Plate.ID] = inputs[i].Container.Plate
+
 	}
+
+	lhr.Input_plates = ip
 
 	return lhr
 }
