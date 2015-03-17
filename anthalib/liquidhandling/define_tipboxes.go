397b09e14e0723294ba1d81a613d93cefc927b23
Michael Sadowski <m.sadowski@synthace.com>
2015-03-17 16:46:01 +0000

    It's a bodge, but it will do for now

--- anthalib/liquidhandling/define_tipboxes.go
+++ anthalib/liquidhandling/define_tipboxes.go
@@ -1,14 +1,15 @@
 package liquidhandling
 
-func Define_Tipboxes(lhr *LHRequest, lhp *LHProperties) {
+func Define_Tipboxes(lhr *LHRequest, lhp *LHProperties) *LHRequest {
 	// make tips
 	// this needs to be refactored elsewhere
-	tipboxes := make([]*liquidhandling.LHTipbox, 2, 2)
-	tip := liquidhandling.NewLHTip("ACMEliquidhandlers", "ACMEliquidhandlers250", 20.0, 250.0)
+	tipboxes := make([]*LHTipbox, 2, 2)
+	tip := NewLHTip("ACMEliquidhandlers", "ACMEliquidhandlers250", 20.0, 250.0)
 	for i := 0; i < 2; i++ {
-		tb := liquidhandling.NewLHTipbox(8, 12, "ACMEliquidhandlers", tip)
+		tb := NewLHTipbox(8, 12, "ACMEliquidhandlers", tip)
 		tipboxes[i] = tb
 	}
 	lhr.Tips = tipboxes
 
+	return lhr
 }
