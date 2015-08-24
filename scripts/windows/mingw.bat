7d0ea77640a6ee37e059179f87df35386d5271a1
ddn0 <d.nguyen@synthace.com>
2015-08-24 16:33:58 -0500

    update mingw url

--- scripts/windows/mingw.bat
+++ scripts/windows/mingw.bat
@@ -5,11 +5,12 @@ if not exist "%TEMP%\7z920-x64.msi" (
 msiexec /qb /i "%TEMP%\7z920-x64.msi"
 
 pushd "%TEMP%"
-rem Make sure the '%' in URL are escaped when copy-and-pasting in
+rem Make sure the '%' nd '&' in URL are escaped when copy-and-pasting in
 if not exist "%TEMP%\mingw.7z" (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://cznic.dl.sourceforge.net/project/mingw-w64/Toolchains%%20targetting%%20Win64/Personal%%20Builds/mingw-builds/5.1.0/threads-posix/seh/x86_64-5.1.0-release-posix-seh-rt_v4-rev0.7z', 'mingw.7z')" <NUL
+  rem Use curl because it can follow redirects
+  "%PROGRAMFILES(X86)%\Git\bin\curl.exe" -L "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%%20targetting%%20Win64/Personal%%20Builds/mingw-builds/5.1.0/threads-posix/seh/x86_64-5.1.0-release-posix-seh-rt_v4-rev0.7z?r=http%%3A%%2F%%2Fsourceforge.net%%2Fprojects%%2Fmingw-w64%%2Ffiles%%2FToolchains%%2520targetting%%2520Win64%%2FPersonal%%2520Builds%%2Fmingw-builds%%2F5.1.0%%2Fthreads-posix%%2Fseh%%2F" -o mingw.7z
 )
-cmd /c ""%PROGRAMFILES%\7-Zip\7z.exe" x mingw.7z -oC:\"
+"%PROGRAMFILES%\7-Zip\7z.exe" x mingw.7z -oC:\
 rename C:\mingw64 MinGW
 rem del mingw.7z
 popd
