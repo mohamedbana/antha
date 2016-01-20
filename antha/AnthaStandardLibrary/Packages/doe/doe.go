693f6a8cdca9b4a5481cf97bf5f3cb5e2e2a6646
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-01-20 16:44:08 +0000

    screen lhpolicies

---
+++ antha/AnthaStandardLibrary/Packages/doe/doe.go
@@ -0,0 +1,68 @@
+// doe.go
+package doe
+
+import (
+	"fmt"
+)
+
+func MakePair(descriptor string, levels []interface{}) (doepair DOEPair) {
+
+	doepair.Factor = descriptor
+	doepair.Levels = levels
+
+	return
+}
+
+func Pair(factordescription string, levels []interface{}) (doepair DOEPair) {
+	doepair.Factor = factordescription
+	doepair.Levels = levels
+	return
+}
+
+type DOEPair struct {
+	Factor string
+	Levels []interface{}
+}
+type Run struct {
+	Factordescriptors []string
+	Setpoints         []interface{}
+}
+
+func (pair DOEPair) LevelCount() (numberoflevels int) {
+	numberoflevels = len(pair.Levels)
+	return
+}
+
+func AllComboCount(pairs []DOEPair) (numberofuniquecombos int) {
+	fmt.Println("In AllComboCount", "len(pairs)", len(pairs))
+	var movingcount int
+	movingcount = (pairs[0]).LevelCount()
+	fmt.Println("levelcount", movingcount)
+	fmt.Println("len(levels)", len(pairs[0].Levels))
+	for i := 1; i < len(pairs); i++ {
+		fmt.Println("levelcount", movingcount)
+		movingcount = movingcount * (pairs[i]).LevelCount()
+	}
+	numberofuniquecombos = movingcount
+	return
+}
+
+func AllCombinations(factors []DOEPair) (runs []Run) {
+	fmt.Println(factors)
+	descriptors := make([]string, 0)
+	setpoints := make([]interface{}, 0)
+	runs = make([]Run, AllComboCount(factors))
+	var run Run
+	for i, factor := range factors {
+		fmt.Println(factor, i, "of", AllComboCount(factors))
+		for j, level := range factor.Levels {
+			fmt.Println(factor, level, i, j, i+j)
+			descriptors = append(descriptors, factor.Factor)
+			setpoints = append(setpoints, level)
+			run.Factordescriptors = descriptors
+			run.Setpoints = setpoints
+			runs[i+j] = run
+		}
+	}
+	return
+}
