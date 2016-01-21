ca00e0fd3eb99a8e2ece884efaf1628f7d9d3ffa
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-01-21 15:13:57 +0000

    updated funcs to read design from dx sheet in an xlsx spreadsheet

--- antha/AnthaStandardLibrary/Packages/doe/doe.go
+++ antha/AnthaStandardLibrary/Packages/doe/doe.go
@@ -3,6 +3,9 @@ package doe
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/antha-lang/antha/antha/AnthaStandardLibrary/Packages/spreadsheet"
 )
 
 type DOEPair struct {
@@ -21,8 +24,12 @@ func Pair(factordescription string, levels []interface{}) (doepair DOEPair) {
 }
 
 type Run struct {
-	Factordescriptors []string
-	Setpoints         []interface{}
+	RunNumber           int
+	StdNumber           int
+	Factordescriptors   []string
+	Setpoints           []interface{}
+	Responsedescriptors []string
+	ResponseValues      []interface{}
 }
 
 func AllComboCount(pairs []DOEPair) (numberofuniquecombos int) {
@@ -56,5 +63,104 @@ func AllCombinations(factors []DOEPair) (runs []Run) {
 			runs[i+j] = run
 		}
 	}
+	return
+}
+
+func RunsFromDXDesign(xlsx string) (runs []Run, err error) {
+	file, err := spreadsheet.OpenFile(xlsx)
+	if err != nil {
+		return runs, err
+	}
+	sheet := spreadsheet.Sheet(file, 0)
+
+	runs = make([]Run, 0)
+	var run Run
+
+	var setpoint interface{}
+	var descriptor string
+	for i := 3; i < sheet.MaxRow; i++ {
+		//maxfactorcol := 2
+		factordescriptors := make([]string, 0)
+		responsedescriptors := make([]string, 0)
+		setpoints := make([]interface{}, 0)
+		responsevalues := make([]interface{}, 0)
+
+		run.RunNumber, err = sheet.Cell(i, 1).Int()
+		if err != nil {
+			return runs, err
+		}
+		run.StdNumber, err = sheet.Cell(i, 0).Int()
+		if err != nil {
+			return runs, err
+		}
+
+		for j := 2; j < sheet.MaxCol; j++ {
+			factororresponse := sheet.Cell(0, j).String()
+			fmt.Println(i, j, factororresponse)
+			if strings.Contains(factororresponse, "Factor") {
+				//	maxfactorcol = j
+				descriptor = strings.Split(sheet.Cell(1, j).String(), ":")[1]
+				factrodescriptor := descriptor
+				fmt.Println(i, j, descriptor)
+
+				cell := sheet.Cell(i, j)
+
+				celltype := cell.Type()
+
+				if celltype == 1 {
+					setpoint, err = cell.Float()
+				} else if celltype == 3 {
+					setpoint = cell.Bool()
+				} else {
+					setpoint = cell.String()
+				}
+				factordescriptors = append(factordescriptors, factrodescriptor)
+				setpoints = append(setpoints, setpoint)
+
+			} else {
+				//run.Factordescriptors = factordescriptors
+
+				//for k := maxfactorcol; k < sheet.MaxCol; k++ {
+				//	factororresponse := sheet.Cell(0, maxfactorcol).String()
+				if strings.Contains(factororresponse, "Response") {
+					descriptor = sheet.Cell(1, j).String()
+					responsedescriptor := descriptor
+					fmt.Println("response", i, j, descriptor)
+					responsedescriptors = append(responsedescriptors, responsedescriptor)
+
+					cell := sheet.Cell(i, j)
+
+					if cell == nil {
+
+						break
+					}
+
+					celltype := cell.Type()
+
+					if celltype == 1 {
+						responsevalue, err := cell.Float()
+						if err != nil {
+							return runs, err
+						}
+						responsevalues = append(responsevalues, responsevalue)
+					} else {
+						responsevalue := cell.String()
+						responsevalues = append(responsevalues, responsevalue)
+					}
+
+				}
+
+			}
+		}
+		run.Factordescriptors = factordescriptors
+		run.Responsedescriptors = responsedescriptors
+		run.Setpoints = setpoints
+		run.ResponseValues = responsevalues
+
+		runs = append(runs, run)
+		factordescriptors = make([]string, 0)
+		responsedescriptors = make([]string, 0)
+	}
+
 	return
 }
