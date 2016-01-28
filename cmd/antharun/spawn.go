34c2d78e6e4905b8a4cf8031be0653aede62152d
ddn0 <d.nguyen@synthace.com>
2016-01-28 10:41:24 +0000

    go get for -package too

--- cmd/antharun/spawn.go
+++ cmd/antharun/spawn.go
@@ -27,24 +27,40 @@ func (a *spawned) Close() (err error) {
 }
 
 func spawn(gopackage string, port int) (*spawned, error) {
-	if f, err := ioutil.TempFile("", "server"); err != nil {
-		return nil, err
-	} else if err := exec.Command("go", "build", "-o", f.Name(), gopackage).Run(); err != nil {
-		return nil, err
-	} else {
-		prefix := ansi.Color("server", "red:white") + " "
-		cmd := exec.Command(f.Name(), "-port", strconv.Itoa(port))
-		w1 := NewWriter(colorable.NewColorableStdout(), prefix)
-		w2 := NewWriter(colorable.NewColorableStderr(), prefix)
-		cmd.Stdout = w1
-		cmd.Stderr = w2
-		s := &spawned{
-			Command: cmd,
-			URI:     fmt.Sprintf("localhost:%d", port),
-		}
-		s.closers = append(s.closers, func() error { return os.Remove(f.Name()) })
-		s.closers = append(s.closers, func() error { return w1.Flush() })
-		s.closers = append(s.closers, func() error { return w2.Flush() })
-		return s, nil
+	runCmd := func(prog string, args ...string) error {
+		c := exec.Command(prog, args...)
+		c.Stdin = os.Stdin
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		return c.Run()
 	}
+
+	if err := runCmd("go", "get", gopackage); err != nil {
+		return nil, err
+	}
+
+	f, err := ioutil.TempFile("", "server")
+	if err != nil {
+		return nil, err
+	}
+	s := &spawned{}
+	s.closers = append(s.closers, func() error { return os.Remove(f.Name()) })
+
+	if err := runCmd("go", "build", "-o", f.Name(), gopackage); err != nil {
+		return s, err
+	}
+
+	prefix := ansi.Color("server", "red:white") + " "
+	w1 := NewWriter(colorable.NewColorableStdout(), prefix)
+	w2 := NewWriter(colorable.NewColorableStderr(), prefix)
+
+	cmd := exec.Command(f.Name(), "-port", strconv.Itoa(port))
+	cmd.Stdout = w1
+	cmd.Stderr = w2
+
+	s.Command = cmd
+	s.URI = fmt.Sprintf("localhost:%d", port)
+	s.closers = append(s.closers, func() error { return w1.Flush() })
+	s.closers = append(s.closers, func() error { return w2.Flush() })
+	return s, nil
 }
