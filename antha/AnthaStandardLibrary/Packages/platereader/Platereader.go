b3d5d818faa06702762ca39081fc98636fd8163d
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-02-12 16:48:30 +0000

    fixed AddplateReaderData element

--- antha/AnthaStandardLibrary/Packages/platereader/Platereader.go
+++ antha/AnthaStandardLibrary/Packages/platereader/Platereader.go
@@ -55,7 +55,7 @@ func Blankcorrect(blank wtype.Absorbance, sample wtype.Absorbance) (blankcorrect
 	return
 }
 
-func EstimatePathLength(plate wtype.LHPlate, volume wunit.Volume) (pathlength wunit.Length, err error) {
+func EstimatePathLength(plate *wtype.LHPlate, volume wunit.Volume) (pathlength wunit.Length, err error) {
 
 	if plate.Welltype.Bottom == 0 /* i.e. flat */ && plate.Welltype.Shape().LengthUnit == "mm" {
 		wellarea, err := plate.Welltype.CalculateCrossSectionArea()
