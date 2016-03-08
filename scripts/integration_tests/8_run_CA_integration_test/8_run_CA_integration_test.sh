1e1a5abc75c668b98f751d93293239aeb902aa07
Michael Sadowski <m.sadowski@synthace.com>
2016-03-08 11:59:44 +0000

    Fixed integration test script

--- scripts/integration_tests/8_run_CA_integration_test/8_run_CA_integration_test.sh
+++ scripts/integration_tests/8_run_CA_integration_test/8_run_CA_integration_test.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
-#drivers=("manual" "go://github.com/Synthace/PipetMaxDriver" "go://github.com/Synthace/CyBioXMLDriver")
-drivers=("manual" "50051")
+drivers=("manual" "go://github.com/Synthace/PipetMaxDriver" "go://github.com/Synthace/CyBioXMLDriver")
+#drivers=("manual" "50051")
 
 for driver in ${drivers[@]}; do
 	s="$GOPATH/src/github.com/antha-lang/antha/cmd/antharun/antharun --workflow 8_run_xplatform_workflow.json --parameters 8_run_xplatform_parameters.json"
 	
-	if [ "$driver" != "manual" ]; then
-		# start the driver
-		($GOPATH/src/github.com/Synthace/PipetMaxDriver/server/server --port $driver --out integration_test.sqlite)&
-		pid=$!
-		s="$s --driver :$driver"
-	fi
+#	if [ "$driver" != "manual" ]; then
+#		# start the driver
+#		($GOPATH/src/github.com/Synthace/PipetMaxDriver/server/server --port $driver --out integration_test.sqlite)&
+#		pid=$!
+#		s="$s --driver :$driver"
+#	fi
 
 	echo $s
 	echo `$s`
 
-	if [ "$driver" != "manual" ]; then
-		echo "PID IS $pid"
-		kill -9 $pid
-	fi
+#	if [ "$driver" != "manual" ]; then
+#		echo "PID IS $pid"
+#		kill -9 $pid
+#	fi
 done
