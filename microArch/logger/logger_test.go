83aa031178ca516e9df11fc3e4af07ac27e6f6a8
ddn0 <d.nguyen@synthace.com>
2016-04-04 11:29:19 +0100

    logger.Fatal should die

--- microArch/logger/logger_test.go
+++ microArch/logger/logger_test.go
@@ -1,22 +1,22 @@
 // microArch/logger/logger_test.go: Part of the Antha language
 // Copyright (C) 2015 The Antha authors. All rights reserved.
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-// 
+//
 // For more information relating to the software or licensing issues please
-// contact license@antha-lang.org or write to the Antha team c/o 
+// contact license@antha-lang.org or write to the Antha team c/o
 // Synthace Ltd. The London Bioscience Innovation Centre
 // 2 Royal College St, London NW1 0NH UK
 
@@ -77,18 +77,35 @@ func TestMiddlewareCalls(t *testing.T) {
 	Debug("")
 	Warning("")
 	Error("")
-	Fatal("")
-	if l := logCounter; l != 5 {
-		t.Error("On Log. Expecting call count ", 5, " got ", l)
+	if l := logCounter; l != 4 {
+		t.Errorf("expected %d log calls, got %d", 4, l)
 	}
 
 	Measure("")
 	if l := measureCounter; l != 1 {
-		t.Error("On Measure. Expecting call count ", 1, " got ", l)
+		t.Errorf("expected %d measure calls, got %d", 1, l)
 	}
 	Sensor("")
 	if l := sensorCounter; l != 1 {
-		t.Error("On Sensor. Expecting call count ", 1, " got ", l)
+		t.Error("expecting %d sensor calls, got %d", 1, l)
+	}
+}
+
+func TestFatal(t *testing.T) {
+	cleanMiddleware()
+	panicked := false
+	aTest := func() {
+		defer func() {
+			if res := recover(); res != nil {
+				panicked = true
+				return
+			}
+		}()
+		Fatal("")
+	}
+	aTest()
+	if !panicked {
+		t.Error("expecting panic")
 	}
 }
 
@@ -110,7 +127,6 @@ func TestMiddlewareCallsSync(t *testing.T) {
 			Debug("")
 			Warning("")
 			Error("")
-			Fatal("")
 			Measure("")
 			Sensor("")
 			wg.Done()
@@ -118,8 +134,8 @@ func TestMiddlewareCallsSync(t *testing.T) {
 	}
 
 	wg.Wait()
-	if l := logCounter; l != 5*syncCount {
-		t.Error("On Log. Expecting call count ", 5*syncCount, " got ", l)
+	if l := logCounter; l != 4*syncCount {
+		t.Error("On Log. Expecting call count ", 4*syncCount, " got ", l)
 	}
 	if l := measureCounter; l != 1*syncCount {
 		t.Error("On Measure. Expecting call count ", 1*syncCount, " got ", l)
