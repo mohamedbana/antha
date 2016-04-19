1bf863b754ad5cf5c89a647ef526ce3b330608f2
jmanart <j.manzanaresartolazabal@gmail.com>
2016-04-19 14:36:51 +0100

    Refactor to use the new Data call

--- microArch/logger/middleware/handler/handler.go
+++ microArch/logger/middleware/handler/handler.go
@@ -1,22 +1,22 @@
 // microArch/logger/middleware/handler/handler.go: Part of the Antha language
 // Copyright (C) 2015 The Antha authors. All rights reserved.
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-// 
+//
 // For more information relating to the software or licensing issues please
-// contact license@antha-lang.org or write to the Antha team c/o 
+// contact license@antha-lang.org or write to the Antha team c/o
 // Synthace Ltd. The London Bioscience Innovation Centre
 // 2 Royal College St, London NW1 0NH UK
 
@@ -30,18 +30,21 @@ import (
 type LogHandlerFunc func(logger.LogLevel, int64, string, string, ...interface{})
 type MeasureHandlerFunc func(int64, string, string, ...interface{})
 type SensorHandlerFunc func(int64, string, string, ...interface{})
+type DataHandlerFunc func(int64, interface{}, ...interface{})
 
 type Handler struct {
 	l LogHandlerFunc
 	t MeasureHandlerFunc
 	s SensorHandlerFunc
+	d DataHandlerFunc
 }
 
-func NewHandler(l LogHandlerFunc, t MeasureHandlerFunc, s SensorHandlerFunc) *Handler {
+func NewHandler(l LogHandlerFunc, t MeasureHandlerFunc, s SensorHandlerFunc, d DataHandlerFunc) *Handler {
 	ret := new(Handler)
 	ret.l = l
 	ret.t = t
 	ret.s = s
+	ret.d = d
 	return ret
 }
 
@@ -63,5 +66,12 @@ func (h Handler) Measure(ts int64, origin string, message string, extra ...inter
 func (h Handler) Sensor(ts int64, origin string, message string, extra ...interface{}) {
 	if h.s != nil {
 		h.s(ts, origin, message, extra)
+	}
+}
+
+//Data log arbitrary structures of data and extra information
+func (h Handler) Data(ts int64, data interface{}, extra ...interface{}) {
+	if h.s != nil {
+		h.d(ts, data, extra)
 	}
 }
