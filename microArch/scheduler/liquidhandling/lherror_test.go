1d1320f97e65f757739205f7e1800c7b5257373e
Michael Sadowski <m.sadowski@synthace.com>
2016-05-25 14:26:45 +0100

    Added test for excess tip box addition error

--- microArch/scheduler/liquidhandling/lherror_test.go
+++ microArch/scheduler/liquidhandling/lherror_test.go
@@ -4,6 +4,10 @@ import (
 	//"github.com/antha-lang/antha/microArch/driver/liquidhandling"
 	"fmt"
 	"testing"
+
+	"github.com/antha-lang/antha/antha/anthalib/wtype"
+	"github.com/antha-lang/antha/antha/anthalib/wunit"
+	"github.com/antha-lang/antha/microArch/factory"
 )
 
 func GetLiquidHandlerForTest() *Liquidhandler {
@@ -36,8 +40,18 @@ func TestDeckSpace1(t *testing.T) {
 
 	lh := GetLiquidHandlerForTest()
 
-	for i := 0; i < lh.Properties.Nposns; i++ {
-		tb := factory.GetTipBoxByType()
+	for i := 0; i < len(lh.Properties.Tip_preferences); i++ {
+		tb := factory.GetTipBoxByTip(lh.Properties.Tips[0])
+		err := lh.Properties.AddTipBox(tb)
+		if err != nil {
+			t.Fatal(fmt.Sprintf("Should be able to fill deck with tipboxes, only managed %d", i+1))
+		}
+	}
+
+	tb := factory.GetTipBoxByTip(lh.Properties.Tips[0])
+	err := lh.Properties.AddTipBox(tb)
+	if err.Error() != "1 (LH_ERR_NO_DECK_SPACE) : insufficient deck space to fit all required items; this may be due to constraints : Trying to add tip box" {
+		t.Fatal(fmt.Sprint("Expected error : 1 (LH_ERR_NO_DECK_SPACE) : insufficient deck space to fit all required items; this may be due to constraints : Trying to add tip box\n", " got: ", err.Error()))
 	}
 }
 
