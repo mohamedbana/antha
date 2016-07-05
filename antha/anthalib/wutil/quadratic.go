1b4805d2bbee482f99d23a45d05b353fa9ad0396
Michael Sadowski <m.sadowski@synthace.com>
2016-07-05 14:12:37 +0100

    Added quadratic

---
+++ antha/anthalib/wutil/quadratic.go
@@ -0,0 +1,27 @@
+package wutil
+
+import (
+	"math"
+	"math/cmplx"
+)
+
+// function for quadratics
+type Quadratic struct {
+	a float64
+	b float64
+	c float64
+}
+
+func (c *Quadratic) A() float64 {
+	return c.a
+}
+func (c *Quadratic) B() float64 {
+	return c.b
+}
+func (c *Quadratic) C() float64 {
+	return c.c
+}
+
+func (c *Quadratic) F(v float64) float64 {
+	return c.a*math.Pow(v, 2.0) + c.b*v + c.c
+}
