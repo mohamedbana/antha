a4053c4572eacac70bc643fba6b31624aa96dc79
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-07-26 17:09:49 +0100

    increase well bottom height

--- microArch/factory/make_plate_library.go
+++ microArch/factory/make_plate_library.go
@@ -244,7 +244,7 @@ func makePlateLibrary() map[string]*wtype.LHPlate {
 	wellyoffset = 4.5               //centre of well to centre of neighbouring well in y direction
 	xstart = -2.5                   // distance from top left side of plate to first well
 	ystart = -2.5                   // distance from top left side of plate to first well
-	zstart = riserheightinmm + 0.25 // offset of bottom of deck to bottom of well
+	zstart = riserheightinmm + 0.75 // offset of bottom of deck to bottom of well
 
 	square = wtype.NewShape("box", "mm", 4, 4, 14)
 	//func NewLHWell(platetype, plateid, crds, vunit string, vol, rvol float64, shape *Shape, bott int, xdim, ydim, zdim, bottomh float64, dunit string) *LHWell {
