2ada943088efc0963379dbfb97ae3b43812cfce8
Xander Anderson <x.anderson@synthace.com>
2016-08-25 14:13:42 +0100

    updated dna lhpolicy postmix

--- microArch/driver/liquidhandling/makelhpolicy.go
+++ microArch/driver/liquidhandling/makelhpolicy.go
@@ -550,8 +550,8 @@ func MakeDNAPolicy() LHPolicy {
 	dnapolicy["DSPZOFFSET"] = 0.5
 	dnapolicy["TIP_REUSE_LIMIT"] = 0
 	//dnapolicy["NO_AIR_DISPENSE"] = true
-	dnapolicy["RESET_OVERRIDE "] = true
-	dnapolicy["POST_MIX_VOLUME"] = 5
+	dnapolicy["RESET_OVERRIDE"] = true
+	dnapolicy["POST_MIX_VOLUME"] = 5.0
 	dnapolicy["POST_MIX"] = 1
 	dnapolicy["POST_MIX_Z"] = 0.5
 	dnapolicy["POST_MIX_RATE"] = 3.0
