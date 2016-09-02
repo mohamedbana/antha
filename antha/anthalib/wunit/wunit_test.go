4f07598c36bf5b4602227ce55f9288fc53694612
Haydn King <h.king@synthace.com>
2016-09-02 16:00:53 +0100

    Update wunit test (unit output is cleaner now)

--- antha/anthalib/wunit/wunit_test.go
+++ antha/anthalib/wunit/wunit_test.go
@@ -137,11 +137,11 @@ func ExampleEight() {
 	fmt.Println(meas3.Unit().ToString())
 	fmt.Println(pu3.PrefixedSymbol())
 	// Output:
-	// 10.000GHz  is  10   GHz
-	// 50.000MHz  is  0.05   GHz
-	// 50.000MHz  is  50   MHz
-	// 10.000GHz  is  10000   MHz
-	// 10.000l
+	// 10GHz  is  10   GHz
+	// 50MHz  is  0.05   GHz
+	// 50MHz  is  50   MHz
+	// 10GHz  is  10000   MHz
+	// 10l
 	// Name: litre Symbol: l Conversion: 1    BaseUnit: l
 	// l
 }
