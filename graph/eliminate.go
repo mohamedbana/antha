a5358c86c48c1908f5a498545ec4d126cc9aea61
ddn0 <d.nguyen@synthace.com>
2016-10-17 17:45:30 +0100

    support any elimination order

--- graph/eliminate.go
+++ graph/eliminate.go
@@ -6,64 +6,98 @@ type EliminateOpt struct {
 	KeepMultiEdges bool
 }
 
+// Elimination can be quadratic. Reduce to average cost by processing nodes in
+// topological order.
+func eliminationOrder(graph Graph) (nodes []Node) {
+	order, err := TopoSort(TopoSortOpt{
+		Graph: graph,
+	})
+	if err == nil {
+		nodes = order
+	} else {
+		for i, inum := 0, graph.NumNodes(); i < inum; i += 1 {
+			nodes = append(nodes, graph.Node(i))
+		}
+	}
+
+	return
+}
+
+func inNodes(graph Graph) map[Node][]Node {
+	ins := make(map[Node][]Node)
+	for i, inum := 0, graph.NumNodes(); i < inum; i += 1 {
+		n := graph.Node(i)
+		for j, jnum := 0, graph.NumOuts(n); j < jnum; j += 1 {
+			out := graph.Out(n, j)
+			ins[out] = append(ins[out], n)
+		}
+	}
+	return ins
+}
+
 // Return graph resulting from node elimination. Node elimination removes node
 // n by adding edges (in(n), out(n)) for the product of incoming and outgoing
 // neighbors.
 func Eliminate(opt EliminateOpt) Graph {
+	// Cache nodes to keep
 	kmap := make(map[Node]bool)
-	keep := func(n Node) bool {
-		r, ok := kmap[n]
-		if !ok {
-			r = opt.In(n)
-			kmap[n] = r
-		}
-		return r
+	for i, inum := 0, opt.Graph.NumNodes(); i < inum; i += 1 {
+		n := opt.Graph.Node(i)
+		kmap[n] = opt.In(n)
 	}
 
+	// Retarget ins of eliminated nodes to outs of eliminated nodes
+	nodes := eliminationOrder(opt.Graph)
+	ins := inNodes(opt.Graph)
+	outs := make(map[Node][]Node)
+	eliminated := make(map[Node]bool)
+	for idx, n := range nodes {
+		// Remove processed ins as we go
+		if idx > 0 {
+			delete(ins, nodes[idx-1])
+		}
+
+		if kmap[n] {
+			continue
+		}
+
+		// Make sure to process outs given to us by previously eliminated nodes
+		nouts := outs[n]
+		for j, jnum := 0, opt.Graph.NumOuts(n); j < jnum; j += 1 {
+			nouts = append(nouts, opt.Graph.Out(n, j))
+		}
+
+		for _, out := range nouts {
+			if kmap[out] {
+				// Normal case: link ins of eliminated node to outs of
+				// eliminated node
+				for _, in := range ins[n] {
+					outs[in] = append(outs[in], out)
+				}
+				continue
+			}
+
+			if eliminated[out] {
+				continue
+			}
+
+			// If we aren't keeping out and we haven't yet processed it, update
+			// its ins to include our ins
+			for _, in := range ins[n] {
+				ins[out] = append(ins[out], in)
+			}
+		}
+		eliminated[n] = true
+	}
+
+	// Create eliminated graph
 	ret := &qgraph{
 		Outs: make(map[Node][]Node),
 	}
 
-	// Elimination can be quadratic. Reduce to linear on special case of trees
-	// with reverse topological order.
-	var nodes []Node
-	if order, err := TopoSort(TopoSortOpt{
-		Graph: opt.Graph,
-	}); err != nil {
-		for i, inum := 0, opt.Graph.NumNodes(); i < inum; i += 1 {
-			nodes = append(nodes, opt.Graph.Node(i))
-		}
-	} else {
-		for i := len(order) - 1; i >= 0; i -= 1 {
-			nodes = append(nodes, order[i])
-		}
-	}
-
-	ins := make(map[Node][]Node)
-	for _, n := range nodes {
-		for j, jnum := 0, opt.Graph.NumOuts(n); j < jnum; j += 1 {
-			out := opt.Graph.Out(n, j)
-			ins[out] = append(ins[out], n)
-		}
-	}
-
-	outs := make(map[Node][]Node)
-	for _, n := range nodes {
-		if keep(n) {
-			continue
-		}
-		for j, jnum := 0, opt.Graph.NumOuts(n); j < jnum; j += 1 {
-			out := opt.Graph.Out(n, j)
-			for _, in := range ins[n] {
-				outs[in] = append(outs[in], out)
-				ins[out] = append(ins[out], in)
-			}
-		}
-	}
-
 	// Filter out nodes
 	for _, n := range nodes {
-		if !keep(n) {
+		if !kmap[n] {
 			continue
 		}
 
@@ -72,14 +106,14 @@ func Eliminate(opt EliminateOpt) Graph {
 		seen := make(map[Node]bool)
 		for j, jnum := 0, opt.Graph.NumOuts(n); j < jnum; j += 1 {
 			dst := opt.Graph.Out(n, j)
-			if !keep(dst) {
+			if !kmap[dst] {
 				continue
 			}
 			seen[dst] = true
 			ret.Outs[n] = append(ret.Outs[n], dst)
 		}
 		for _, dst := range outs[n] {
-			if !keep(dst) {
+			if !kmap[dst] {
 				continue
 			}
 			if !opt.KeepMultiEdges && seen[dst] {
